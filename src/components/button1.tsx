d55fdadd6cd48ddd09211eaffcffdc637d4f00e8
vadim_budarin <budarin-vv@yandex.ru>
2020-12-09 01:53:43 +0300

    fix: code

--- src/components/button1.tsx
+++ src/components/button1.tsx
@@ -1,5 +1,4 @@
 import { compose } from '@bem-react/core';
 import { Button as ButtonDesktop, withSizeM, withViewDefault } from '@yandex/ui/Button/desktop';
-import '@yandex/ui/Button/Button.css';
 
 export const Button1 = compose(withSizeM, withViewDefault)(ButtonDesktop);
