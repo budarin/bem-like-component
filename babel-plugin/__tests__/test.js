e1e019e1ffeeb51a5e13bb7f124e9481dd78cdad
vadim_budarin <budarin-vv@yandex.ru>
2020-12-12 00:03:15 +0300

    fix: tests

--- babel-plugin/__tests__/test.js
+++ babel-plugin/__tests__/test.js
@@ -2,11 +2,10 @@ import path from 'path';
 import pluginTester from 'babel-plugin-tester';
 import BemButtonPlugin from '../bubel-plugin-bem-button';
 
-const fixtures = path.join(__dirname, 'fixtures');
 const getFixtures = (fixtureFolder) => ({
     title: fixtureFolder.replace('-', ' '),
-    fixture: path.join(fixtures, `${fixtureFolder}/input.js`),
-    outputFixture: path.join(fixtures, `${fixtureFolder}/output.js`),
+    fixture: path.join(__dirname, `fixtures/${fixtureFolder}/input.js`),
+    outputFixture: path.join(__dirname, `/fixtures${fixtureFolder}/output.js`),
 });
 
 pluginTester({
