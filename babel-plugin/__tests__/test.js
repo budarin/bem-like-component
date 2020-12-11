a47ec80ccb7f765a4cbdf2ba93812067fa1a96fd
vadim_budarin <budarin-vv@yandex.ru>
2020-12-12 00:02:24 +0300

    fix: refactor tests

--- babel-plugin/__tests__/test.js
+++ babel-plugin/__tests__/test.js
@@ -2,13 +2,15 @@ import path from 'path';
 import pluginTester from 'babel-plugin-tester';
 import BemButtonPlugin from '../bubel-plugin-bem-button';
 
+const fixtures = path.join(__dirname, 'fixtures');
+const getFixtures = (fixtureFolder) => ({
+    title: fixtureFolder.replace('-', ' '),
+    fixture: path.join(fixtures, `${fixtureFolder}/input.js`),
+    outputFixture: path.join(fixtures, `${fixtureFolder}/output.js`),
+});
+
 pluginTester({
     plugin: BemButtonPlugin,
     pluginName: 'bubel-plugin-bem-button',
-    tests: [
-        {
-            fixture: path.join(__dirname, 'fixtures/input.js'),
-            outputFixture: path.join(__dirname, 'fixtures/output.js'),
-        },
-    ],
+    tests: [{ ...getFixtures('simple-usage') }],
 });
