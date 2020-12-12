8fd38ce26cbe74acd0b0cbad98ea03da706006dc
vadim_budarin <budarin-vv@yandex.ru>
2020-12-12 17:14:03 +0300

    fix: code

--- babel-plugin/bubel-plugin-bem-button.js
+++ babel-plugin/bubel-plugin-bem-button.js
@@ -41,7 +41,11 @@ module.exports = declare((api, options) => {
         visitor: {
             ImportDeclaration: {
                 enter(path) {
-                    if (path.node.specifiers.length > 0 && path.node.specifiers[0].imported.name === 'createButton') {
+                    if (
+                        path.node.specifiers.length > 0 &&
+                        path.node.specifiers[0].imported &&
+                        path.node.specifiers[0].imported.name === 'createButton'
+                    ) {
                         path.replaceWith(bemCoreImport());
                         path.skip();
                     }
