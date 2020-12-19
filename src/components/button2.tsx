a2be9e27938806d286de7983c2806eba47df077c
vadim_budarin <budarin-vv@yandex.ru>
2020-12-19 20:49:05 +0300

    fix: types

--- src/components/button2.tsx
+++ src/components/button2.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@yandex/ui/Button/desktop/bundle';
 
-export const Button2 = ({ children, ...rest }: { children: React.ReactNode }) => (
+export const Button2 = ({ children, ...rest }: React.ComponentProps<typeof Button>) => (
     <Button {...rest} size="m" view="action" width="max">
         {children}
     </Button>
