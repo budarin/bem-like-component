6ec7bcb3b64a42d8b93c10302bc0030e6aa4fb95
vadim_budarin <budarin-vv@yandex.ru>
2020-12-19 21:17:31 +0300

    fix: cmd

--- config/webpack/build.sh
+++ config/webpack/build.sh
@@ -1,1 +1,1 @@
-NODE_ENV=production webpack --mode production --config ./config/webpack/webpack.config.js
+rimraf .eslintcache && rimraf ./dist && rimraf node_modules/.cache && NODE_ENV=production webpack --mode production --config ./config/webpack/webpack.config.js
--- src/components/button2.tsx
+++ src/components/button2.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@yandex/ui/Button/desktop/bundle';
 
-export const Button2 = ({ children, ...rest }: React.ComponentProps<typeof Button>) => (
+export const Button2 = ({ children, ...rest }: { children: React.ReactNode }) => (
     <Button {...rest} size="m" view="action" width="max">
         {children}
     </Button>
